746c772fdecdd8de7c32e03ac5ebbedb1b902ba4
DeboraTilaar <deboraciusuwarni.tilaar@my.jcu.edu.au>
2017-05-17 19:16:28 +0800

    Adding about-us.php and contact-us.php
    Adding about-us.php and contact-us.php. Change some css in new.css and
    global.css

---
+++ specialcreations/wp-content/themes/cp3402-team4-customtheme/about-us.php
@@ -0,0 +1,14 @@
+<?php
+get_header(); ?>
+
+<div class="about-us-content">
+    <div class="navigation-container"></div>
+    <div class="container">
+        <div class="display-title">About Us</div>
+        <div class="details">
+            Paper Creation is a company which provides many types of personal handmade gift. We operate all around the world through this online store and well as a physical store at Sims Drive. An example of our service would be customers can pick from a wide selection of gifts they want, pick the color they prefer, and we will make the gift with those specifications in a short period of time. Paper Creations aims to help people who want to personally make a gift to their special person.
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
---
+++ specialcreations/wp-content/themes/cp3402-team4-customtheme/contact-us.php
@@ -0,0 +1,21 @@
+<?php
+get_header(); ?>
+
+    <div class="contact-us-content">
+        <div class="navigation-container"></div>
+        <div class="container">
+            <div class="display-title">Contact Us</div>
+            <div class="details">
+                Address : 149 Sims Drive, 387380<br />
+                Email : paperCreation@gmail.com<br />
+                Phone : +65 6709 3888<br />
+                Opening Hours :<br />
+                Monday - Friday : 09.00 - 22.00<br />
+                Saturday - Sunday : 10.30 - 22.00<br />
+            </div>
+            <div class="map">
+            </div>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
