1de34f01e6df4f6054311fa1fc32901cc76b3e24
EdwinChooWL <edwin.choo@my.jcu.edu.au>
2017-05-19 16:35:53 +0800

    - Added about-us as a template page

--- specialcreations/wp-content/themes/cp3402-team4-customtheme/about-us.php
+++ specialcreations/wp-content/themes/cp3402-team4-customtheme/about-us.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Template Name: About Us Template
+ */
 get_header(); ?>
 <div class="about-us-content">
     <div class="title">
@@ -11,7 +14,7 @@ get_header(); ?>
     <div class="container">
         <div class="display-title">About Us</div>
         <div class="details">
-            Paper Creation is a company which provides many types of personal handmade gift. We operate all around the world through this online store and well as a physical store at Sims Drive. An example of our service would be customers can pick from a wide selection of gifts they want, pick the color they prefer, and we will make the gift with those specifications in a short period of time. Paper Creations aims to help people who want to personally make a gift to their special person.
+            <?php get_the_content() ?>
         </div>
     </div>
 
