00319363aeff0004ad44324bb101d81547a57750
EdwinChooWL <edwin.choo@my.jcu.edu.au>
2017-05-22 09:32:32 +0800

    Change of files directory

---
+++ specialcreations/wp-content/themes/cp3402-team4-customtheme/header.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package CP3402-Team4-Custom-Theme
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+    <header class="navigation">
+        <div class="container">
+            <?php wp_nav_menu( array(
+                'container' => 'div',
+                'container_class' => 'nav-bar',
+            ) ); ?>
+            <div class="search">
+                <?php
+                $search_bar = get_template_directory() . '/woocommerce/product-searchform.php';
+                require_once ( $search_bar );
+                ?>
+            </div>
+        </div>
+    </header><!-- header end -->
+	<div id="content" class="site-content">
+        <div class="title">
+            <div class="container">
+                <div class="maintitle">
+                    <?php echo '<a href="new.html">' . get_theme_mod( "main_title_text" ) . '</a>' ?>
+                </div>
+                <div class="subtitle">
+                    <?php echo get_theme_mod( "sub_title_text" ) ?>
+                </div>
+            </div>
+        </div>
+        <div class="title-container"></div>
